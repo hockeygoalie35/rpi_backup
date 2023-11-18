bc435f253a806acb93c378cb8bf67a470216e2e7
hockeygoalie35 <hockeygoalie35@samulenas.com>
2023-11-18 01:12:07 -0500

    Split Cronjob to separate setup file

--- setup.sh
+++ setup.sh
@@ -7,12 +7,3 @@ deactivate
 
 
 
-# (Optional) Set up Cronjob to run at set interval
-# Currently set to midnight on the first of each month, but change it for your needs
-#                Mn Hr DoM Mon DoW
-#(crontab -l ; echo "0  0 1 * * bash $HOME/rpi_backup/run.sh >> $HOME/rpi_backup/cron.log") | crontab -
-#echo "# Log for Backups running through Cron" >> cron.log
-#echo "---------------------------------------" >> cron.log
-
-
-
