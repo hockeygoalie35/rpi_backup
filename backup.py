817708b74ef89f16393669945181128414961c22
hockeygoalie35 <hockeygoalie35@samulenas.com>
2023-11-21 13:35:28 -0500

    Updated backup.py to class. Broke some funcs away to pi_functions.py, as it can be used in other projects.

--- backup.py
+++ backup.py
@@ -1,132 +1,140 @@
-# Python 3.11.2
-# Usage: Raspberry Pi SD Card live backups using Image-Utils by RonR.
-# Attaches to a network drive of your choosing.
-
 import os
+import sys
 import subprocess
 from colorama import init, Fore
 import datetime
 import json
 import traceback
-import sys
+import argparse
+from pi_functions import *
 
 
 
 
-def log_error(log_string):
-    print(Fore.RED + datetime.datetime.strftime(datetime.datetime.now(),'%D-%H:%M:%S') + " " + log_string)
-    with open(f'{script_directory}/python_log.txt', 'a') as savefile:
-        savefile.write("------------------------------------\n"+datetime.datetime.strftime(datetime.datetime.now(),'%D-%H:%M:%S') + ":\n " + log_string + "\n")
-        savefile.close()
-def log(log_string):
-    print(Fore.GREEN+ datetime.datetime.strftime(datetime.datetime.now(),'%D-%H:%M:%S') + " " + log_string)
-    with open(f'{script_directory}/python_log.txt', 'a') as savefile:
-        savefile.write(datetime.datetime.strftime(datetime.datetime.now(),'%D-%H:%M:%S') + ": " + log_string + "\n")
-        savefile.close()
 
-def run_backup():
-    init(autoreset=True)  # Set autoreset to True else color would be forwarded to the next print statement
+class rpi_backup():
+    def __init__(self):
+        self.script_directory = os.path.dirname(os.path.abspath(sys.argv[0]))
+        init(autoreset=True)  # Set autoreset to True else color would be forwarded to the next print statement
+        self.argument_parsing()
+        if self.argument.setup:
+            credentials_dict = {
+                "-networkpath": self.argument.networkpath,
+                "-user": self.argument.user,
+                "-password": self.argument.password,
+                "-uid": self.argument.uid}
+            mnt_path = '/mnt/backups'
+            print(Fore.GREEN + '\n\n########|STARTING SETUP|#########')
+            self.check_networks_drive_credentials(credentials_dict)
+            create_cifs_drive(credentials_dict['-networkpath'], mnt_path, credentials_dict['-user'], credentials_dict['-password'])
+            print(Fore.GREEN + '#########|SETUP Complete|#########\n\n')
+        if self.argument.runbackup:
+            print(Fore.GREEN + '\n\n########|STARTING BACKUP|#########')
+            self.run_backup()
+            print(Fore.GREEN + '########|BACKUP COMPLETE|#########')
+        if self.argument.enablecron:
+            self.enable_cron()
+        if self.argument.disablecron:
+            self.disable_cron()
 
-    # Windows Share
-    # path E.g //192.168.1.5/E/Folder/Folder2/etc...
-    # Username and Pass are Windows account
-    # uid = raspberry pi username...usually pi
-
-    # Try to load credentials, if not, create the file and ask user to fill
-    try:
-        with open(f'{script_directory}/creds.json') as openfile:
-            # Reading from json file
-            creds = json.load(openfile)  # loads in the credential dict
-            openfile.close()
-    except:
-        creds = {"path": "","username": "","password": "","uid": ""}
-        creds = json.dumps(creds)
-        with open(f'{script_directory}/creds.json', 'w') as savefile:
-            savefile.write(creds)
-        log_error("creds.json created.")
-        exit(0)
-    path=creds["path"]
-    username=creds["username"]
-    password=creds["password"]
-    uid = creds["uid"]
-
-
-    # Ensure Image-Utils was downloaded and ask user to do so if not
-    if os.path.exists(f'{script_directory}/image-utils/image-backup') is False:
-        log_error("Please Download Image-Utils and place in this folder /rpi_backup/image-utils\nhttps://forums.raspberrypi.com/viewtopic.php?t=332000")
-        exit("missing utils")
-    # Backup Settings
-    filesize_buffer = 5000
-    incremental_size = 0
-
-    fstab_string = f'{path} /mnt/backups cifs username={username},password={password},uid={uid}'
-    log( f"Backup Starting....")
+    def argument_parsing(self):
+        parser = argparse.ArgumentParser(description="CLI Commands")
+        parser.add_argument("-s", "--setup",help="initial setup, supply path, username, password and uid if user is not pi",required=False, default=False, action='store_true')
+        parser.add_argument("-rb", "--runbackup", help="run backup", required=False, default=False, action='store_true')
+        parser.add_argument("-ec", "--enablecron", help="enables cronjob to schedule backup", required=False,default=False, action='store_true')
+        parser.add_argument("-dc", "--disablecron", help="enables cronjob to schedule backup", required=False,default=False, action='store_true')
+        parser.add_argument("-networkpath", help="path to network share", required=False, default=False)
+        parser.add_argument("-user", help="network share username", required=False, default=False)
+        parser.add_argument("-password", help="network share password", required=False, default=False)
+        parser.add_argument("-uid", help="path to network share", required=False, default=False)
+        self.argument = parser.parse_args()
+    def enable_cron(self): # TODO Test this func to make sure it works
+        os.system(f'(crontab -l ; echo "0 0 1 * * sudo {self.script_directory}/rpi_backup_venv/bin/python {self.script_directory}/backup.py >> {self.script_directory}/logs/cron.log") | crontab -')
+        os.system(f'echo "# Log for Backups running through Cron" > {self.script_directory}/logs/cron.log')
+        os.system(f'echo "---------------------------------------" >> {self.script_directory}/logs/cron.log')
+        print("cronjob enabled")
+    def disable_cron(self):
+        os.system(f"crontab -l | grep -v 'sudo {self.script_directory}/rpi_backup_venv/bin/python {self.script_directory}/backup.py'  | crontab -")
+        print("cronjob disabled")
 
 
-    # Create Mount point on pi if needed
-    with open('/etc/fstab') as fstab:
-        if  os.path.exists('/mnt/backups') is False or fstab_string not in fstab.read():
-            if os.path.exists('/mnt/backups') is False:
-                try:
-                    os.mkdir('/mnt/backups')
-                    log( "Created mounting point")
-                except:
-                    log(Fore.RED + "Could not create mounting point. See above error")
-                    exit(1)
-            if fstab_string not in fstab.read():
-                os.system(f"sudo su -c \"echo '{fstab_string}' >> /etc/fstab\"")
-                os.system("sudo systemctl daemon-reload")
-                log( "Created drive mount link")
-    fstab.close()
+    def check_networks_drive_credentials(self,credentials_dict):
 
-    # Mount network drive
-    os.system("sudo mount /mnt/backups")
-    log( "Backup Drive Mounted")
-
-    # Get hostname and see if directory exists
-    cmd = "hostname | cut -d\' \' -f1"
-    hostname = str(subprocess.check_output(cmd, shell=True).decode('utf-8')).replace("\n","")
-    if os.path.exists(f'/mnt/backups/{hostname}') is False:
-        log( f"Creating Directory {hostname}")
-        os.mkdir(f'/mnt/backups/{hostname}')
-
-    # Get Filesystem size
-    cmd = "df -h /|awk '/\/dev\//{printf(\"%.2f\",$3);}'"
-    file_size_gb = str(subprocess.check_output(cmd,shell=True).decode('utf-8')).replace("\n","")
-    file_size_mb  = float(file_size_gb) * 1000
-
-    # Build up back-up args
-    filesystem_size = int(file_size_mb + filesize_buffer)
-
-    # Disable Docker
-    log( "Docker disabled (Temporarily)")
-    os.system("sudo systemctl disable docker --now")
-    os.system("sudo systemctl disable docker.socket --now")
-    os.system(f"sudo bash /home/{uid}/rpi_backup/image-utils/image-backup -i /mnt/backups/{hostname}/{hostname}_$(date +%d-%b-%y_%T).img,{filesystem_size},{incremental_size}")
+        # let user know if they're missing a flag
+        stop_code = False
+        for cred in credentials_dict:
+            if cred != "-uid": # ignore uid, it's optional
+                if credentials_dict[cred] is False:
+                    log('e',f'missing {cred} argument')
+                    stop_code = True
+        if stop_code is True:
+            exit(0)
 
 
 
-    # Unmount network drive
-    os.system("sudo umount /mnt/backups")
-    log( "Backup Drive Unmounted")
-
-    # Enable Docker
-    os.system("sudo systemctl enable docker --now")
-    os.system("sudo systemctl enable docker.socket --now")
-    log( "Docker Re-enabled")
 
 
-    log( f"Backup Completed")
-    log( "\n--------------------------------------------------")
+    def run_backup(self):
+
+        # Ensure Image-Utils was downloaded and ask user to do so if not
+        if os.path.exists(f'{script_directory}/image-utils/image-backup') is False:
+            log('e',"Please Download Image-Utils and place in this folder /rpi_backup/image-utils\nhttps://forums.raspberrypi.com/viewtopic.php?t=332000")
+            exit(1)
+        # Backup Settings
+        filesize_buffer = 5000
+        incremental_size = 0
+
+        log('i',"Backup Starting....")
+
+        # Mount network drive
+        os.system("sudo mount /mnt/backups")
+        mount_list = str(subprocess.run('mount -l', shell=True, stdout=subprocess.PIPE).stdout.decode("utf-8"))
+        if '/mnt/backups' in mount_list:
+            log('s',"Backup Drive Mounted")
+        else:
+            log('e',"Mount Error! See above error.")
+
+
+        # Get hostname and see if directory exists
+        cmd = "hostname | cut -d\' \' -f1"
+        hostname = str(subprocess.check_output(cmd, shell=True).decode('utf-8')).replace("\n", "")
+        if os.path.exists(f'/mnt/backups/{hostname}') is False:
+            log('i',f"Creating Directory {hostname}")
+            os.system('sudo mkdir /mnt/backups/{hostname}')
+
+        # Get Filesystem size
+        cmd = "df -h /|awk '/\/dev\//{printf(\"%.2f\",$3);}'"
+        file_size_gb = str(subprocess.check_output(cmd, shell=True).decode('utf-8')).replace("\n", "")
+        file_size_mb = float(file_size_gb) * 1000
+        log('i', f"File System Size: {file_size_mb}MB")
+
+        # Build up back-up args
+        filesystem_size = int(file_size_mb + filesize_buffer)
+
+        # Get uid
+        uid = str(subprocess.check_output('whoami', shell=True).decode('utf-8')).replace("\n", "")
+        log('i',f'Identified user as {uid}')
+        # Disable Docker
+        log('i',"Docker disabled (Temporarily)")
+        os.system("sudo systemctl disable docker.socket --now")
+        os.system("sudo systemctl disable docker --now")
+        log('i', "Beginning image-backup")
+        # # backup string
+        os.system(f"sudo bash /home/{uid}/rpi_backup/image-utils/image-backup -i /mnt/backups/{hostname}/{hostname}_$(date +%d-%b-%y_%T).img,{filesystem_size},{incremental_size}")
+        log('i', "image-backup Finishing")
+        # Unmount network drive
+        os.system("sudo umount /mnt/backups")
+        log('s', "Backup Drive Unmounted")
+
+        # Enable Docker
+        os.system("sudo systemctl enable docker --now")
+        os.system("sudo systemctl enable docker.socket --now")
+        log('i', "Docker Re-enabled")
+
+        log('s',"Backup Completed")
+        log('i',"\n--------------------------------------------------")
+
+
 
 if __name__ == '__main__':
-    # Get Script directory
-    script_directory = os.path.dirname(os.path.abspath(sys.argv[0]))
-    print(script_directory)
-
-    try:
-        run_backup()
-    except Exception:
-        log_error(traceback.format_exc())
-        exit(1)
-
+    a=rpi_backup()
